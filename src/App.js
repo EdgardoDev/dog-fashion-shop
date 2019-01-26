5bcf1f27f7aeb4022ec1b6631f535ab4113d705c
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-26 12:43:29 +0100

    Created the different components for this application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
-    return <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <span><i className="fas fa-home"></i></span>
-          </div>
-          <div className="col-6">COLUMN 2</div>
-        </div>
-      </div>;
+    return (
+    <React.Fragment>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
+    </React.Fragment>
+    );
   }
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Nabvar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>HELLO FROM NABVAR</h3>
+      </div>
+    )
+  }
+}
