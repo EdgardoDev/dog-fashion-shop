04cd6115841ed9fe39da86e659ad8301d2b942c4
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-26 23:45:43 +0100

    Added links to the navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
@@ -10,13 +11,15 @@ import Default from "./components/Default";
 class App extends Component {
   render() {
     return (
-    <React.Fragment>
+      <React.Fragment>
         <Navbar />
-        <ProductList />
-        <Details />
-        <Cart />
-        <Default />
-    </React.Fragment>
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={ Details } />
+          <Route path="/cart" component={ Cart } />
+          <Route component={ Default } />
+        </Switch>
+      </React.Fragment>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Nabvar extends Component {
   render() {
-    return (
-      <div>
-        <h3>HELLO FROM NABVAR</h3>
-      </div>
-    )
+    return <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+        <a className="navbar-brand" href="/">
+            <i class="fas fa-dog fa-lg mr-1" /> DOG FASHION SHOP
+          </a>
+        </Link>
+
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+          <Link to="/" className="nav-link">
+            PRODUCTS
+          </Link>
+          </li>
+        </ul>
+
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus mr-1" />
+            MY CART
+          </button>
+        </Link>
+      </nav>;
   }
 }
