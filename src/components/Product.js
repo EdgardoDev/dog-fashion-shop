806f63692ec941b69598d7bc7a30d543db9bae6a
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-28 17:37:35 +0100

    Defined PropTypes on the Product component to avoid problems with data.

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
+import PropTypes from 'prop-types';
 
 
 export default class Product extends Component {
@@ -45,6 +46,17 @@ export default class Product extends Component {
     )
   }
 }
+
+// Here we define the PropTypes
+Product.propTypes = {
+  product: PropTypes.shape ({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool
+  }).isRequired
+};
 
 const ProductWrapper = styled.div`
   .card {
