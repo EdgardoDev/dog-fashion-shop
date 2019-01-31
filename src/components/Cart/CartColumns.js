8f16bcdb1239502c21396fc56de33acbb8c351c8
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-01-31 22:24:11 +0100

    CartItem component and its logic has been created

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -1,29 +1,27 @@
 import React from 'react'
 
 export default function CartColumns() {
-  return (
-    <div className="container-fluid text-center d-none d-lg-block">
-      <div className="row">
+  return <div className="container-fluid text-center d-none d-lg-block">
+      <div className="row my-4">
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">product image</p>
+          <p className="text-uppercase"><i class="fas fa-images" /> product image</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">product name</p>
+          <p className="text-uppercase"><i class="fas fa-file-signature"></i> product name</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">price</p>
+          <p className="text-uppercase"><i class="fas fa-dollar-sign"></i> price</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">quantity</p>
+          <p className="text-uppercase"><i class="fas fa-list-ol"></i> quantity</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">remove</p>
+          <p className="text-uppercase"><i class="fas fa-minus-circle"></i> remove</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">total</p>
+          <p className="text-uppercase"><i class="fas fa-poll-h"></i> total</p>
         </div>
       </div>
-    </div>
-  )
+    </div>;
 }
 
