88234f456dd3304dc36911d2ffc0bfe3fcea0949
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2019-02-02 12:00:40 +0100

    Functionality for the page not found component was added

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,9 +3,20 @@ import React, { Component } from 'react'
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>404 PAGE NOT FOUND...</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h2 className="display-3">404</h2>
+            <h2>error</h2>
+            <h3>page not found</h3>
+            <h1 className="my-3">
+              <i class="fas fa-exclamation-triangle fa-lg" />
+            </h1>
+            <h3>the requested url <span className="text-danger">
+            {this.props.location.pathname}</span> was not found</h3>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
